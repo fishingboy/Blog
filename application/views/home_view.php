955fe228e6f338b9a2df9b0f1bc3b0a31b692336
Leo.Kuo <et282523@hotmail.com>
2020-08-25 01:33:22 +0800

    修改 title

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
-<title>Welcome to CodeIgniter</title>
+<title>Leo's Blog</title>
 <link rel="stylesheet" type="text/css" href="/public/css/global.css"/>
 
 <style type="text/css">
